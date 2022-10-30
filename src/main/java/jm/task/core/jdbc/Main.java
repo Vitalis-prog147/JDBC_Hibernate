a457baec9d4a0a522e77deeb19027303e095a7ea
Vitalis-prog147 <vitalis.programming28@gmail.com>
2022-10-30 14:45:44 +0300

    Ver 2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Name_1", "LastName_1", (byte) 20);
+        userDaoJDBC.saveUser("Name_2", "LastName_2", (byte) 25);
+        userDaoJDBC.saveUser("Name_3", "LastName_3", (byte) 31);
+        userDaoJDBC.saveUser("Name_4", "LastName_4", (byte) 38);
+        System.out.println(userDaoJDBC.getAllUsers());
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/users";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static Connection connection;
+
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        return configuration.buildSessionFactory();
+    }
 }
