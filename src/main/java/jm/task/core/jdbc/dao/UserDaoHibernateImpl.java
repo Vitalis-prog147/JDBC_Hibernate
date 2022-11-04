327623ad7376fd86a24451cd6193f39ee80de0ea
Vitalis-prog147 <vitalis.programming28@gmail.com>
2022-11-04 16:21:49 +0300

    Version hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,142 @@
 package jm.task.core.jdbc.dao;
 
+import jakarta.persistence.criteria.CriteriaQuery;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory = Util.getConnection();
+
     public UserDaoHibernateImpl() {
 
     }
-
-
+    @SuppressWarnings("deprecation")
     @Override
     public void createUsersTable() {
+        try {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
 
+            session.createNativeQuery("""
+                    CREATE TABLE IF NOT EXISTS users.users (
+                    id int not null auto_increment, name VARCHAR(50),
+                    lastname VARCHAR(50),
+                    `age` INT NOT NULL,
+                    PRIMARY KEY (id))
+                    """).executeUpdate();
+            transaction.commit();
+            System.out.println("Table has been created");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void dropUsersTable() {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            System.out.println("Table has been created");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+            System.out.printf("User с именем – %s добавлен в базу данных\n", name);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void removeUserById(long id) {
-
+        Session session = Util.getConnection().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+            System.out.println("User has been deleted");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        /**
+         * PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users");
+         ResultSet resultSet = preparedStatement.executeQuery();
+         while (resultSet.next()) {
+         User user = new User();
+         user.setId(resultSet.getLong("userId"));
+         user.setName(resultSet.getString("name"));
+         user.setLastName(resultSet.getString("last_name"));
+         user.setAge(resultSet.getByte("age"));
+         list.add(user);
+         */
+
+        Session session = Util.getConnection().openSession();
+        CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder().createQuery(User.class);
+        criteriaQuery.from(User.class);
+        Transaction transaction = session.beginTransaction();
+        List<User> userList = session.createQuery(criteriaQuery).getResultList();
+        try {
+            transaction.commit();
+            return userList;
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return userList;
     }
 
+    @SuppressWarnings("deprecation")
+    @Override
+    public void cleanUsersTable() throws SQLException {
+
+    }
+/**
     @Override
     public void cleanUsersTable() {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.createNativeQuery("DELETE FROM users.users").executeUpdate();
+            transaction.commit();
+            System.out.println("Table was cleaned");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
     }
+    */
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,30 +2,63 @@ package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/users";
-    private static final String USERNAME = "root";
+    /**
+     * private static final String URL = "jdbc:mysql://localhost:3306/users";
+     * private static final String USERNAME = "root";
+     * private static final String PASSWORD = "root";
+     * private static Connection;
+     * <p>
+     * <p>
+     * public static Connection getConnection() {
+     * try {
+     * Class.forName("com.mysql.cj.jdbc.Driver");
+     * connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+     * } catch (SQLException | ClassNotFoundException e) {
+     * e.printStackTrace();
+     * }
+     * return connection;
+     * }
+     * <p>
+     * public static SessionFactory getSessionFactory() {
+     * Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+     * return configuration.buildSessionFactory();
+     * }
+     */
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql://localhost:3306/users";
+    private static final String LOGIN = "root";
     private static final String PASSWORD = "root";
-    private static Connection connection;
+    private static SessionFactory sessionFactory = null;
 
-
-    public static Connection getConnection() {
+    public static SessionFactory getConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", HOST)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return sessionFactory;
+
+    }
     public static SessionFactory getSessionFactory() {
         Configuration configuration = new Configuration().addAnnotatedClass(User.class);
         return configuration.buildSessionFactory();
